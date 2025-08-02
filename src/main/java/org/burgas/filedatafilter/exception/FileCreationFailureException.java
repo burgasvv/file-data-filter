e8b7b3f8e3676f2857a7f8182a3e3a3863503364
Burgas <burgassme@gmail.com>
2025-08-02 12:03:05 +0700

    Тестирование

--- src/main/java/org/burgas/filedatafilter/exception/FileCreationFailureException.java
+++
@@ -1,8 +0,0 @@
-package org.burgas.filedatafilter.exception;
-
-public class FileCreationFailureException extends RuntimeException {
-
-    public FileCreationFailureException(String message) {
-        super(message);
-    }
-}
